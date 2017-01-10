0a51564352d97377633dcb0c21e9fd9d04f0ec7e
henry <iswanghui@qq.com>
2017-01-10 13:49:14 +0800

    add spring redis test

--- src/test/java/com/demo/ApplicationTests.java
+++ src/test/java/com/demo/ApplicationTests.java
@@ -1,9 +1,11 @@
 package com.demo;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -15,6 +17,9 @@ public class ApplicationTests {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Test
     public void sendSimpleMail() throws Exception {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -22,10 +27,20 @@ public class ApplicationTests {
         message.setTo("2504377865@qq.com");
         message.setSubject("主题：简单邮件");
         message.setText("测试邮件内容");
-        mailSender.send(message);
+        //mailSender.send(message);
     }
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void RedisTest() throws Exception {
+        // 保存字符串
+        stringRedisTemplate.opsForValue().set("aaa", "111");
+        Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+
+        String str = stringRedisTemplate.opsForValue().get("aaa");
+        System.out.println(str);
     }
 }
